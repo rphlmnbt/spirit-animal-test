7883d8a0a73df81e04e34ac7ad66abd9a1bb82ee
Raphael Manabat <raphael.manabat@hoolisoftware.com>
2024-06-12 09:23:59 +0800

    Integrated api for results page

--- BE/controllers/mlController.py
+++ BE/controllers/mlController.py
@@ -1,12 +1,14 @@
 import json
 
 from flask_restful import request
+from flask_cors import cross_origin
 from __main__ import app
 import numpy as np
 from services.mlService import MLService
 
 
 @app.route('/ml', methods=['POST'])
+@cross_origin(origin='*')
 def predict():
     ml = MLService()
     json = request.json
@@ -16,6 +18,7 @@ def predict():
 
 
 @app.route('/ml/probability', methods=['POST'])
+@cross_origin(origin='*')
 def predict_probability():
     ml = MLService()
     json = request.json
--- FE/brown-bag/src/pages/questionnaire/index.js
+++ FE/brown-bag/src/pages/questionnaire/index.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import './style.css'
 import { Button, Card, Col, Form, Image, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const Questionnaire = () => {
   const questionArray = [
@@ -37,9 +39,47 @@ const Questionnaire = () => {
     'Use intellect and trickery to overcome obstacles'
   ]
 
-  const [count, setCount] = useState(0)
+  const firstChoiceValues = [
+    'grasslands',
+    'leadership',
+    'headon',
+    'leader',
+    'confront'
+  ]
 
-  const navigate = useNavigate()
+  const secondChoiceValues = [
+    'woodlands',
+    'determination',
+    'tenaciously',
+    'supporter',
+    'persist'
+  ]
+
+  const thirdChoiceValues = [
+    'forests',
+    'intelligence',
+    'strategically',
+    'observer',
+    'intellect'
+  ]
+
+  const features = [
+    'environment',
+    'characteristic',
+    'challenge_approach',
+    'role',
+    'adversity_reaction'
+  ]
+
+  const [count, setCount] = useState(0)
+  const [environment, setEnvironment] = useState('')
+  const [characteristic, setCharacteristic] = useState('')
+  const [challengeApproach, setChallengeApproach] = useState('')
+  const [role, setRole] = useState('')
+  const [adversityReaction, setAdversityReaction] = useState('')
+  const [probabilities, setProbabilities] = useState([])
+  const [result, setResult] = useState('')
+  const [showResults, setShowResults] = useState(false)
 
 
   const onNext = () => {
@@ -50,45 +90,129 @@ const Questionnaire = () => {
     count > 0 && setCount(count - 1)
   }
 
+  const onSelect = (event) => {
+    features[count] === 'environment' && setEnvironment(event.target.value)
+    features[count] === 'characteristic' && setCharacteristic(event.target.value)
+    features[count] === 'challenge_approach' && setChallengeApproach(event.target.value)
+    features[count] === 'role' && setRole(event.target.value)
+    features[count] === 'adversity_reaction' && setAdversityReaction(event.target.value)
+  }
+  const getResults = (answers) => {
+    axios.post('http://localhost:8081/ml/probability', {
+      answers: answers,
+    })
+    .then(function (response) {
+      console.log(response);
+      setProbabilities(response.data.probabilities)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    axios.post('http://localhost:8081/ml', {
+      answers: answers,
+    })
+    .then(function (response) {
+      console.log(response);
+      setResult(response.data.result[0])
+      setShowResults(true)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  }
+
+  
+
   return (
-    <Container className="container">
+    <Container className="main-container">
       <Card className="card">
-        <Card.Body>
-          <Card.Title className="question">{questionArray[count]}</Card.Title>
-          <hr></hr>
-          <Card.Text className="answer">
-            <Form.Check
-              type='radio'
-              label={' ' + firstChoiceArray[count]}
-            />
-          </Card.Text>
-          <Card.Text className="answer">
-            <Form.Check
-              type='radio'
-              label={' ' + secondChoiceArray[count]}
-            />
-          </Card.Text>
-          <Card.Text className="answer">
-            <Form.Check
-              type='radio'
-              label={' ' + thirdChoiceArray[count]}
-            />
-          </Card.Text>
-          <div className="btn-container">
-            {
-              count > 0 &&
-              <Button variant="primary" className="back" onClick={onBack}>
-                BACK
-              </Button>
-            }
-            {
-              count < 4 &&
-              <Button variant="primary" className="next" onClick={onNext}>
-                NEXT
-              </Button>
-            }
-          </div>
-        </Card.Body>
+        {
+          showResults === false &&
+          <Card.Body>
+            <Card.Title className="question">{questionArray[count]}</Card.Title>
+            <hr></hr>
+            <Card.Text className="answer">
+              <Form.Check
+                type='radio'
+                value={firstChoiceValues[count]}
+                label={' ' + firstChoiceArray[count]}
+                name={features[count]}
+                onClick={onSelect}
+              />
+            </Card.Text>
+            <Card.Text className="answer">
+              <Form.Check
+                type='radio'
+                value={secondChoiceValues[count]}
+                label={' ' + secondChoiceArray[count]}
+                name={features[count]}
+                onClick={onSelect}
+              />
+            </Card.Text>
+            <Card.Text className="answer">
+              <Form.Check
+                type='radio'
+                value={thirdChoiceValues[count]}
+                label={' ' + thirdChoiceArray[count]}
+                name={features[count]}
+                onClick={onSelect}
+              />
+            </Card.Text>
+            <div className="btn-container">
+              {
+                count > 0 &&
+                <Button className="btn-custom-secondary" onClick={onBack}>
+                  BACK
+                </Button>
+              }
+              {
+                count < 4 &&
+                <Button className="btn-custom" onClick={onNext}>
+                  NEXT
+                </Button>
+              }
+              {
+                count == 4 &&
+                <Button className="btn-custom" onClick={event => getResults([environment, characteristic, challengeApproach, role, adversityReaction])}>
+                  SUBMIT
+                </Button>
+              }
+            </div>
+          </Card.Body>
+        }
+        {
+          showResults === true &&
+          <Card.Body>
+            <Card.Title className="resultTitle">Your Spirit Animal is the {result}!</Card.Title>
+            <hr></hr>
+            <Card.Text style={{display: 'flex', alignItems: 'center'}}>
+              <Col xs={2} >
+                <span>Beaver</span>
+              </Col>
+              <Col xs={10}>
+                <ProgressBar variant="warning" className="success" now={probabilities[0]*100} label={`${(probabilities[0]*100).toFixed(2)}%`} />
+              </Col>
+            </Card.Text>
+            <Card.Text style={{display: 'flex', alignItems: 'center'}}>
+              <Col xs={2} >
+                <span>Raven</span>
+              </Col>
+              <Col xs={10}>
+                <ProgressBar className="success"  now={probabilities[2]*100} label={`${(probabilities[2]*100).toFixed(2)}%`} />
+              </Col>
+            </Card.Text>
+            <Card.Text style={{display: 'flex', alignItems: 'center'}}>
+              <Col xs={2} >
+                <span>Lion</span>
+              </Col>
+              <Col xs={10}>
+                <ProgressBar variant="danger" className="success"  now={probabilities[1]*100} label={`${(probabilities[1]*100).toFixed(2)}%`} />
+              </Col>
+            </Card.Text>
+          </Card.Body>
+        }
       </Card>
       
     </Container>
